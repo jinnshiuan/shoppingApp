da5c68b97a5d591f7163f8c75f31e9e54c261cc3
mollyhung <jinnshiuan@gmail.com>
2018-07-21 22:46:59 +0800

    Initial Commit

---
+++ shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shop
+//
+//  Created by Jinnshiuan on 2018/7/21.
+//  Copyright © 2018年 Molly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
